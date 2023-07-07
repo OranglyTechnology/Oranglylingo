962830f84eb07c26677d8cec691df185d1e40d8d
Claudio Soares <claudiolibanez@gmail.com>
2023-07-07 17:17:18 -0300

    feat: update files

--- android/app/src/main/java/com/oranglytech/oranglylingo/MainApplication.java
+++ android/app/src/main/java/com/oranglytech/oranglylingo/MainApplication.java
@@ -17,8 +17,6 @@ import expo.modules.ReactNativeHostWrapper;
 
 import java.util.List;
 
-import io.invertase.firebase.firestore.ReactNativeFirebaseFirestorePackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
